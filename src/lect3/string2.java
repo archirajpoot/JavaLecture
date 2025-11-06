2f1ae885e06b976d34e53d0a9777d7440eb33bbe
archirajpoot <archirajpoot21@gmail.com>
2025-11-06 13:39:03 +0530

    arrayy

---
+++ src/lect3/string2.java
@@ -0,0 +1,46 @@
+package lect3;
+
+public class string2 {
+    public static void main(String[] args){
+//         String s="   hello    ";
+//        System.out.println(s.length());
+//        System.out.println(s.charAt(1));
+//        System.out.println(s.toUpperCase());
+//        System.out.println(s.toLowerCase());
+//        System.out.println(s.trim());
+//          System.out.println(s.substring(2));
+//          System.out.println(s.indexOf(s));
+//        String str="archi";
+//        String str1="archi";
+//        System.out.println(str==str1);
+//        String str=new String("hyy");
+//        String str1=new String("hyy");
+//        System.out.println(str.equals(str1));
+////        System.out.println(str==str1);
+//        String a="madam";
+//        String b="";
+//        for(int i=a.length()-1;i>=0;i--){
+//            b+=a.charAt(i);
+//        }
+//        if(a.equals(b)){
+//            System.out.println("Is palindrome");
+//        }
+//        else{
+//            System.out.println("Not palindrome");
+//        }
+//        String str="Archii Singh Rajput";
+//        String []a=str.split(" ");
+//        for(String val :a){
+//            System.out.println(val);
+//        }
+//        String str="archii";
+//        int v=0;
+//        for(char ch:str.toCharArray()){
+//            if("aeiou".indexOf(ch)!=-1){
+//                v++;
+//            }
+//        }
+//        System.out.println(v);
+
+    }
+}
