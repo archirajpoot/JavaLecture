e8e512b72c000a838ec04b8a1c4af58b9d97240e
archirajpoot <archirajpoot21@gmail.com>
2025-11-11 17:16:31 +0530

    staticc keyword

--- src/lect6/class1.java
+++ src/lect6/class1.java
@@ -1,20 +1,23 @@
 package lect6;
+
 class detail{
-    String name="archi";
-    static void print(){
-        detail d=new detail();
-        System.out.println(d.name);
+     String name="archi";
+    void print(){
+//        detail d=new detail();
+        System.out.println(name);
     }
 }
-/* method bhi nonstatic hai or name bhi toh access rskte hai string bhi statci hai or stirng static nahi toh bhi acess ho jayga*/
+//key pointss
+/* When both the member method and the variable are non static then they can acess each other
+*  when the variable is static and method is non static then they can acess each other
+* when the mehtod is static but the variable is non static then they can acess each oher to resolve this we
+* can create a object within the mehtod of that class to acces the thingss */
 public class class1 {
     public static void main(String []args){
         detail d = new detail();
         d.print();
 //        detail d1=new detail();
 //        d1.print();
-        //. Create a class Student with fields name, rollNumber, and marks. Write a method to display
-        // student details. Create 3 student objects and display their data.
 
     }
 }
--- src/lect6/ques.java
+++ src/lect6/ques.java
@@ -1,7 +1,23 @@
 package lect6;
 
+class student{
+    String name="";
+    int roll=0;
+    int marks=0;
+    void display(){
+        System.out.println(name);
+        System.out.println(roll);
+        System.out.println(marks);
+    }
+}
+
 public class ques {
     public static void main(String[]args){
+     //. Create a class Student with fields name, rollNumber, and marks. Write a method to display
+        // student details. Create 3 student objects and display their data.
+//        student[] s1=new student(3);
+
+
 
     }
 }
