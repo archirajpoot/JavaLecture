b8d675aec6dfa5d63bf2989dbd7526741af2852b
archirajpoot <archirajpoot21@gmail.com>
2025-11-20 18:42:50 +0530

    abstrct

--- src/lect10/abstract2.java
+++ src/lect10/abstract2.java
@@ -3,14 +3,21 @@ abstract class shape{
     abstract void area();
 }
 class circle extends shape{
+    int r;
+    public circle(int r){
+        this.r=r;
+    }
     void area(){
-        int r=2;
         System.out.println(3.14*r*r);
     }
 }
 class rectangle extends shape{
+    int l,b;
+    public rectangle(int l,int b){
+        this.l=l;
+        this.b=b;
+    }
     void area(){
-        int l=2;int b=9;
         System.out.println(l*b);
     }
 }
@@ -19,8 +26,8 @@ public class abstract2 {
         // 1. Create an abstract class 'Shape' with an abstract method 'area()'.
         // Implement two subclasses
         // 'Circle' and 'Rectangle'.
-        shape s=new circle();
-        shape s1=new rectangle();
+        shape s=new circle(2);
+        shape s1=new rectangle(8,9);
         s.area();
         s1.area();
     }
